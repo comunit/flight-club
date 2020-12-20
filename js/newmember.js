abd1e7519701a9c8ea535532af16d40f501a006c
Imran Rafique <comunit@live.com>
2020-12-20 21:45:13 +0000

    implemented error and spinners on sign up page

--- js/newmember.js
+++ js/newmember.js
@@ -2,6 +2,7 @@ var signUpEmail = document.getElementById("email_sign-up");
 var signUpPassword = document.getElementById("create-a-password");
 var NextButton = document.getElementById("next-button");
 var SignUpPTag = document.getElementById("sign-up-p-tag");
+var spinner = document.getElementById("spinner");
 var step = 1;
 
 NextButton.addEventListener("click", function (e) {
@@ -9,12 +10,28 @@ NextButton.addEventListener("click", function (e) {
   if (step == 1) {
     console.log("step-1");
     signUpEmail.style.width = "0";
+    spinner.style.display = "block";
+    NextButton.style.display = "none";
     setTimeout(() => {
       signUpEmail.style.display = "none";
       signUpPassword.style.display = "inline-block";
       SignUpPTag.innerHTML = "Create a password for your account";
       signUpPassword.focus();
+      spinner.style.display = "none";
+      NextButton.style.display = "block";
+      ShowErrorMessage("Your Email already exists please log in");
       step = 2;
     }, 1000);
   }
 });
+
+// call this function with what you wana display as a message
+function ShowErrorMessage(message) {
+  document.getElementById("errordivp").innerHTML = message;
+  document.getElementById("error-div").style.display = "block";
+  spinner.style.display = "none";
+  setTimeout(function () {
+    document.getElementById("error-div").style.display = "none";
+    document.getElementById("errordivp").innerHTML = "";
+  }, 10000);
+}
