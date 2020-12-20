8c2a97d4bfab75d8036a942b94b88b9140abf7b2
Imran Rafique <comunit@live.com>
2020-12-20 17:02:32 +0000

    working on sign up

--- js/newmember.js
+++ js/newmember.js
@@ -2,11 +2,19 @@ var signUpEmail = document.getElementById("email_sign-up");
 var signUpPassword = document.getElementById("create-a-password");
 var NextButton = document.getElementById("next-button");
 var SignUpPTag = document.getElementById("sign-up-p-tag");
+var step = 1;
 
 NextButton.addEventListener("click", function (e) {
   e.preventDefault();
-  signUpEmail.style.display = "none";
-  signUpPassword.style.display = "inline-block";
-  SignUpPTag.innerHTML = "Create a password for your account";
-  signUpPassword.focus();
+  if (step == 1) {
+    console.log("step-1");
+    signUpEmail.style.width = "0";
+    setTimeout(() => {
+      signUpEmail.style.display = "none";
+      signUpPassword.style.display = "inline-block";
+      SignUpPTag.innerHTML = "Create a password for your account";
+      signUpPassword.focus();
+      step = 2;
+    }, 1000);
+  }
 });
