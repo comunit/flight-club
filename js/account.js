2c4f28271350f20b7d3c5ee9ef65119bcdbddfc9
Imran Rafique <comunit@live.com>
2020-12-21 21:23:33 +0000

    working on with account page

--- js/account.js
+++ js/account.js
@@ -1,12 +1,16 @@
 var cats = document.getElementsByClassName("flex-item");
 var backButton = document.getElementsByClassName("back-img");
 var contentContainer = document.getElementsByClassName("content-container");
+var mainH2Img = document.getElementById("main-h2-img");
+var mainWelcomeHeading = document.getElementById("Welcome-main-heading");
 
 for (let i = 0; i < cats.length; i++) {
   const cat = cats[i];
   cat.addEventListener("click", () => {
     document.getElementsByClassName("flex-container")[0].style.display = "none";
     document.getElementById("profile-container").style.display = "flex";
+    mainH2Img.style.display = "block";
+    mainWelcomeHeading.innerHTML = "Profile";
   });
 }
 
@@ -18,6 +22,8 @@ for (let i = 0; i < backButton.length; i++) {
       contentContainers.style.display = "none";
       document.getElementsByClassName("flex-container")[0].style.display =
         "flex";
+      mainWelcomeHeading.innerHTML = "<strong>Welcome</strong> Imran";
+      mainH2Img.style.display = "none";
     }
   });
 }
