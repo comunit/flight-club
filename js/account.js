15191a90b8d0e6c53e2d4c0d3db1e0420c37fd7d
Imran Rafique <comunit@live.com>
2020-12-21 20:50:53 +0000

    working on account page

---
+++ js/account.js
@@ -0,0 +1,23 @@
+var cats = document.getElementsByClassName("flex-item");
+var backButton = document.getElementsByClassName("back-img");
+var contentContainer = document.getElementsByClassName("content-container");
+
+for (let i = 0; i < cats.length; i++) {
+  const cat = cats[i];
+  cat.addEventListener("click", () => {
+    document.getElementsByClassName("flex-container")[0].style.display = "none";
+    document.getElementById("profile-container").style.display = "flex";
+  });
+}
+
+for (let i = 0; i < backButton.length; i++) {
+  const goBackButton = backButton[i];
+  goBackButton.addEventListener("click", () => {
+    for (let i = 0; i < contentContainer.length; i++) {
+      const contentContainers = contentContainer[i];
+      contentContainers.style.display = "none";
+      document.getElementsByClassName("flex-container")[0].style.display =
+        "flex";
+    }
+  });
+}
